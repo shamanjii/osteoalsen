332d26f3995ed2cd31bf77f1dbf2dcbb0b4279f3
Lumos Vitalis <LumosVitalis@Joshuas-MacBook-Pro.local>
2025-05-17 23:41:29 +0200

    update

---
+++ scripts/load-posts.js
@@ -0,0 +1,8 @@
+document.getElementById('posts').innerHTML = `
+  <div class="post">
+    <h2><a href="/blog/erster-artikel.md">Mein erster Beitrag</a></h2>
+    <p><em>2025-05-17</em></p>
+    <img src="/images/beispiel.jpg" alt="">
+    <p>Kurzvorschau des Inhalts...</p>
+  </div>
+`;
